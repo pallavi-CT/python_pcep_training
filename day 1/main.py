f4eb3785f5b9f0eb7dc9728c6990f81c48f5d8be
Pallavi Prasad <pallavi.p@cloudthat.com>
2026-05-25 12:44:35 +0530

    day 1 python

---
+++ day 1/main.py
@@ -0,0 +1,72 @@
+# this is a simple python code
+# Keywords: False, class, finally, is, return, for, else, if, elif
+
+
+
+# print("Hello, World!") 
+
+# message = "Welcome to Python programming!"
+# print(message)
+
+# salary = 50000
+# bonus = 5000
+# tax = 3000
+
+# net_salary = salary + bonus - tax
+# print("Net Salary:", net_salary)
+
+# num = 10
+# binary_num =  bin(num)
+# print("Binary: ", binary_num)
+
+# print(hex(num))
+# 
+# print(int("1010",2))
+# oct()
+
+# encoding and decoding example
+# char = 'D'
+# ascii_value = ord(char)
+# binary_value = bin(ascii_value)
+
+# print("character ", char)
+# print("ascii ", ascii_value)
+# print("binary ", binary_value)
+
+# encoded_val = format(ord(char), '08b')
+# print("Encoded ", encoded_val)
+
+
+#decode
+# decimal_val = int(encoded_val, 2)
+# char_val = chr(decimal_val)
+
+# print("character: ", char_val)
+
+# val = int(input("Enter a number "))
+# print(type(val))
+
+# str1 = "25.5"
+# print(float(str1))
+
+# x = 10
+# print(complex(2,3))
+
+# print(isinstance(x, int))
+
+
+print("Convert Celsuis to Farenheit")
+val = float(input("enter a value in celsius"))
+
+faren = (val *9/5) + 32
+
+print("Celsius: ", val)
+print("Farenheit: ", faren)
+
+PI = 3.14
+
+"""docstring here
+this is the second line
+"""
+
+# tomorrow: operators, control flow statements, array
